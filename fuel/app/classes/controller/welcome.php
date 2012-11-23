6066e637a6511ca8e94d1fb6c40fb2c2eab8b8c7
Caio Franchi <caio.franchi@cobaia.us>
2012-11-23 00:12:41 -0200

    mail

--- fuel/app/classes/controller/welcome.php
+++ fuel/app/classes/controller/welcome.php
@@ -23,6 +23,40 @@ class Controller_Welcome extends Controller
 		return Response::forge(View::forge('welcome/index'));
 	}
 
+    public function action_mail()
+    {
+        \Package::load('email');
+        //
+
+        $email = Email::forge();
+        $email_data = "teste";
+
+        // Set the from address
+        $email->from('anonymous@email.me', 'My Name');
+
+        // Set the to address
+        $email->to('receiver@elsewhere.co.uk', 'Johny Squid');
+
+        // Set a subject
+        $email->subject('This is the subject');
+
+        // Set multiple to addresses
+
+        $email->to(array(
+            'example@mail.com' => 'With a Name',
+        ));
+
+
+        // Set a html body message
+        $email->html_body(\View::forge('email/template', $email_data));
+
+        /** By default this will also generate an alt body from the html,
+        and attach any inline files (not paths like http://...)       **/
+
+// Set an alt body, this is optional.
+        $email->alt_body('This is my alt body, for non-html viewers.');
+    }
+
 	/**
 	 * A typical "Hello, Bob!" type example.  This uses a ViewModel to
 	 * show how to use them.
---
+++ fuel/app/views/email/message.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Caio
+ * Date: 22/11/12
+ * Time: 23:43
+ * To change this template use File | Settings | File Templates.
+ */
+echo "lalalalala";
+?>
